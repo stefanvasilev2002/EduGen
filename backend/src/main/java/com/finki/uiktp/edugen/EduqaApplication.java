dca72dde9effa0742fde109f6f75118ef9803aac
omniscient <vasilevstefan2002@gmail.com>
2025-03-20 10:08:23 +0100

    backend init

---
+++ backend/src/main/java/com/finki/uiktp/edugen/EduqaApplication.java
@@ -0,0 +1,13 @@
+package com.finki.uiktp.eduqa_generator.eduqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduqaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/finki/uiktp/eduqa_generator/eduqa/EduqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.uiktp.eduqa_generator.eduqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
